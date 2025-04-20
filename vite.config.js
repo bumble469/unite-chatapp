7bdf332fb632e3c86f8778561d26cc0e4f368358
bumble469 <bumble469@gmail.com>
2025-04-20 20:51:25 +0530

    convert to app feature

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Unite',
+        short_name: 'Unite',
+        description: 'Real-time chat web app with private chats and public and private rooms.',
+        theme_color: 'rgb(25, 118, 210)',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/unite-logo.jpeg',
+            sizes: '192x192',
+            type: 'image/jpg'
+          },
+          {
+            src: '/icons/unite-logo.jpeg',
+            sizes: '512x512',
+            type: 'image/jpg'
+          }
+        ]
+      }
+    })
+  ]
+});
